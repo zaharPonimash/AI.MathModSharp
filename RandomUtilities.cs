5374f8572ca3e58519c4ef473bdcf7b49b5b0ecf
Zack <ponimashz@mail.ru>
2017-09-17 23:44:59 +0300

    Отделение класса тензора 3-го ранга от нейросети

---
+++ RandomUtilities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AI.MathMod
+{
+    public static class RandomUtilities
+    {
+        private static readonly Random random = new Random();
+        private static double val;
+        private static bool returnVal = false;
+
+        public static int Seed
+        {
+            get { return (int) DateTime.Now.Ticks; }
+        }
+
+        public static double GaussianRandom()
+        {
+           if (returnVal)
+            {
+                returnVal = false;
+                return val;
+            }
+
+            var u = 2*random.NextDouble() - 1;
+            var v = 2*random.NextDouble() - 1;
+            var r = u*u + v*v;
+
+            if (r == 0 || r > 1)
+            {
+                return GaussianRandom();
+            }
+
+            var c = Math.Sqrt(-2*Math.Log(r)/r);
+            val = v*c; 
+            returnVal = true;
+
+            return u*c;
+        }
+
+        public static double Randn(double mu, double std)
+        {
+        	return mu +GaussianRandom()*std;
+        }
+    }
+}
---
+++ Tensor3.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AI.MathMod
+{
+    /// <summary>
+    ///     Представляет тензор 3-го ранга, как базовый клас нейронной сети
+    /// </summary>
+    [Serializable]
+    public class Tensor3
+    {
+        
+        public int Depth;
+        public int Height;
+        public double[] WeightGradients;
+        public double[] Weights;
+        public int Width;
+
+        /// <summary>
+        ///     Заполнение тензора случайными числами
+        /// </summary>
+        /// <param name="width">ширина</param>
+        /// <param name="height">высота</param>
+        /// <param name="depth">глубина</param>
+        public Tensor3(int width, int height, int depth)
+        {
+            
+            this.Width = width;
+            this.Height = height;
+            this.Depth = depth;
+            
+            //Количество элементов в тензоре
+            var n = width * height * depth;
+            this.Weights = new double[n];
+            this.WeightGradients = new double[n];
+
+            // Нормализация веса выполняется для выравнивания дисперсии
+            // выхода каждого нейрона, иначе нейроны с большим количеством
+            // входов будут иметь выходы большей дисперсии
+            var scale = Math.Sqrt(1.0 / (width * height * depth));
+
+            for (var i = 0; i < n; i++)
+            {
+                this.Weights[i] = RandomUtilities.Randn(0.0, scale);
+            }
+        }
+
+        /// <summary>
+        /// Тензор 3-го ранга
+        /// </summary>
+        /// <param name="width">Ширина</param>
+        /// <param name="height">Высота</param>
+        /// <param name="depth">Глубина</param>
+        /// <param name="c">Величина которой инициализируется тензор</param>
+        public Tensor3(int width, int height, int depth, double c)
+        {
+            
+            this.Width = width;
+            this.Height = height;
+            this.Depth = depth;
+
+            var n = width * height * depth;
+            this.Weights = new double[n];
+            this.WeightGradients = new double[n];
+
+            if (c != 0)
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    this.Weights[i] = c;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Инициализация с помощь интерфейса IList<double>
+        /// </summary>
+        /// <param name="weights">Значения</param>
+        public Tensor3(IList<double> weights)
+        {
+            this.Width = 1;
+            this.Height = 1;
+            this.Depth = weights.Count;
+
+            this.Weights = new double[this.Depth];
+            this.WeightGradients = new double[this.Depth];
+
+            for (var i = 0; i < this.Depth; i++)
+            {
+                this.Weights[i] = weights[i];
+            }
+        }
+
+
+        public double Get(int x, int y, int d)
+        {
+            var ix = ((this.Width * y) + x) * this.Depth + d;
+            return this.Weights[ix];
+        }
+
+
+
+
+        public void Set(int x, int y, int d, double v)
+        {
+            var ix = ((this.Width * y) + x) * this.Depth + d;
+            this.Weights[ix] = v;
+        }
+
+        public void Add(int x, int y, int d, double v)
+        {
+            var ix = ((this.Width * y) + x) * this.Depth + d;
+            this.Weights[ix] += v;
+        }
+
+        public double GetGradient(int x, int y, int d)
+        {
+            var ix = ((this.Width * y) + x) * this.Depth + d;
+            return this.WeightGradients[ix];
+        }
+
+        public void SetGradient(int x, int y, int d, double v)
+        {
+            var ix = ((this.Width * y) + x) * this.Depth + d;
+            this.WeightGradients[ix] = v;
+        }
+
+        public void AddGradient(int x, int y, int d, double v)
+        {
+            var ix = ((this.Width * y) + x) * this.Depth + d;
+            this.WeightGradients[ix] += v;
+        }
+
+        public Tensor3 CloneAndZero()
+        {
+            return new Tensor3(this.Width, this.Height, this.Depth, 0.0);
+        }
+
+        public Tensor3 Clone()
+        {
+            var Tensor3 = new Tensor3(this.Width, this.Height, this.Depth, 0.0);
+            var n = this.Weights.Length;
+
+            for (var i = 0; i < n; i++)
+            {
+               Tensor3.Weights[i] = this.Weights[i];
+            }
+
+            return Tensor3;
+        }
+
+        public void AddFrom(Tensor3 Tensor3)
+        {
+            for (var i = 0; i < this.Weights.Length; i++)
+            {
+                this.Weights[i] += Tensor3.Weights[i];
+            }
+        }
+
+        public void AddGradientFrom(Tensor3 Tensor3)
+        {
+            for (var i = 0; i < this.WeightGradients.Length; i++)
+            {
+                this.WeightGradients[i] += Tensor3.WeightGradients[i];
+            }
+        }
+
+        public void AddFromScaled(Tensor3 Tensor3, double a)
+        {
+            for (var i = 0; i < this.Weights.Length; i++)
+            {
+                this.Weights[i] += a * Tensor3.Weights[i];
+            }
+        }
+
+        public void SetConst(double c)
+        {
+            for (var i = 0; i < this.Weights.Length; i++)
+            {
+            	this.Weights[i] += c;
+            }
+        }
+    }
+}
